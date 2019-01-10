b885641170691f7c0e2f1d19400942c9360b3814
essejose <ze_pk2@hotmail.com>
2019-01-09 23:50:36 -0200

    add touch control

--- Assets/Assets/Scripts/Player/Player.cs
+++ Assets/Assets/Scripts/Player/Player.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
 
     public float velocidade ;
+    Animator animator;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        animator = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,10 +21,25 @@ public class Player : MonoBehaviour {
 
     void Movement()
     {
-        Debug.Log(Input.GetAxisRaw("Horizontal"));
-        float mover_x = Input.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
+        Debug.Log(CrossPlatformInputManager.GetAxisRaw("Horizontal"));
+        Debug.Log(CrossPlatformInputManager.GetAxisRaw("Vertical"));
+        float mover_x = CrossPlatformInputManager.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
         transform.Translate(mover_x, 0.0f, 0.0f);
-        float mover_y = Input.GetAxisRaw("Vertical") * velocidade * Time.deltaTime;
+        float mover_y = CrossPlatformInputManager.GetAxisRaw("Vertical") * velocidade * Time.deltaTime;
         transform.Translate(0.0f, mover_y, 0.0f);
+
+
+
+        // animator.SetFloat("walk_top", Mathf.Abs(Input.GetAxisRaw("Vertical")));
+
+        if (mover_x > 0.0f) 
+
+        {
+            animator.SetInteger("dir",1);
+        }
+        else if (mover_x < 0.0f)
+        {
+
+        }
     }
 }
