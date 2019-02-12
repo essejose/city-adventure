af27be84871b1cf8e73404a15384f0dd7bbfc739
essejose <ze_pk2@hotmail.com>
2019-02-11 23:41:58 -0200

    change controller

--- Assets/Assets/Scripts/Player/Player.cs
+++ Assets/Assets/Scripts/Player/Player.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
 
     public float velocidade ;
     Animator animator;
+	public Text sensitive;
 
     // Use this for initialization
     void Start () {
@@ -21,25 +23,43 @@ public class Player : MonoBehaviour {
 
     void Movement()
     {
-        Debug.Log(CrossPlatformInputManager.GetAxisRaw("Horizontal"));
-        Debug.Log(CrossPlatformInputManager.GetAxisRaw("Vertical"));
-        float mover_x = CrossPlatformInputManager.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
+        float mover_x;
+        float mover_y;
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            
+            Debug.Log(Application.platform);
+            mover_x = CrossPlatformInputManager.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
+            mover_y = CrossPlatformInputManager.GetAxisRaw("Vertical") * velocidade * Time.deltaTime;
+        }
+        else
+        {
+            mover_x = Input.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
+            mover_y = Input.GetAxisRaw("Vertical") * velocidade * Time.deltaTime;
+			//Debug.Log(mover_x);
+			//Debug.Log(mover_y);
+        };
+
+
+     
         transform.Translate(mover_x, 0.0f, 0.0f);
-        float mover_y = CrossPlatformInputManager.GetAxisRaw("Vertical") * velocidade * Time.deltaTime;
         transform.Translate(0.0f, mover_y, 0.0f);
 
-
+		Debug.Log( mover_y );
+		sensitive.text = mover_y.ToString();
+		animator.SetFloat ("dirx", mover_x);
+		animator.SetFloat("diry", mover_y);
 
         // animator.SetFloat("walk_top", Mathf.Abs(Input.GetAxisRaw("Vertical")));
 
-        if (mover_x > 0.0f) 
-
-        {
-            animator.SetInteger("dir",1);
-        }
-        else if (mover_x < 0.0f)
-        {
-
-        }
+//        if (mover_x > 0.0f) 
+//
+//        {
+//            animator.SetInteger("dirx",2);
+//        }
+//        else if (mover_x < 0.0f)
+//        {
+//			animator.SetInteger("dirx",-2);
+//        }
     }
 }
