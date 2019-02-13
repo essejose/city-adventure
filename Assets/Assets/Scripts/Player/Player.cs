c607e820b5d24bb097d8365950d9a55a18983803
essejose <ze_pk2@hotmail.com>
2019-02-13 00:29:46 -0200

    fix movimentacao

--- Assets/Assets/Scripts/Player/Player.cs
+++ Assets/Assets/Scripts/Player/Player.cs
@@ -9,8 +9,8 @@ public class Player : MonoBehaviour {
 
     public float velocidade ;
     Animator animator;
-	public Text sensitive;
-
+	public Text Tmover_y;
+	public Text Tmover_x;
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
@@ -40,15 +40,43 @@ public class Player : MonoBehaviour {
 			//Debug.Log(mover_y);
         };
 
+		 animator.SetFloat ("diry", 0f);
+		 animator.SetFloat ("dirx", 0f);
 
+		if (mover_y > 0.007f)
+		{
+			Debug.Log("Moving Up");
+			animator.SetFloat ("diry",mover_y);
+		}
+
+		else if (mover_y < -0.007f)
+		{
+			Debug.Log("Moving Down");
+			animator.SetFloat ("diry", mover_y);
+		}
+
+		if (mover_x > 0.007f)
+		{
+			Debug.Log("Moving Right");
+			animator.SetFloat ("dirx", mover_x);
+		}
+
+		else if (mover_x < -0.007f)
+		{
+			Debug.Log("Moving left");
+			animator.SetFloat ("dirx", mover_x);
+		}
      
         transform.Translate(mover_x, 0.0f, 0.0f);
         transform.Translate(0.0f, mover_y, 0.0f);
 
-		Debug.Log( mover_y );
-		sensitive.text = mover_y.ToString();
-		animator.SetFloat ("dirx", mover_x);
-		animator.SetFloat("diry", mover_y);
+
+		Tmover_y.text = " move y: "+ mover_y.ToString();
+		Tmover_x.text = " move x: "+mover_x.ToString();
+	
+
+		//animator.SetFloat ("diry", mover_y);
+		//animator.SetFloat ("dirx", mover_x);
 
         // animator.SetFloat("walk_top", Mathf.Abs(Input.GetAxisRaw("Vertical")));
 
